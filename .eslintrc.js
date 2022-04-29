2f331496dae63d477af7b75026cc281aa13e2edb
EthanJiao <ethajiao@foxmail.com>
2022-04-29 20:18:25 +0800

    style: 初始化

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,24 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+      node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ["plugin:vue/essential", "eslint:recommended"],
   parserOptions: {
-    parser: '@babel/eslint-parser'
+      parser: "@babel/eslint-parser",
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+      "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+      "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+      // "vue/multi-word-component-names": 'off',
+      'space-before-function-paren': 'off',
+      overrides: [{
+          files: ["src/components/Hello.vue", "src/components/**/Hello.vue"], // 匹配 components 和二级目录中的 Hello.vue
+          rules: {
+              "vue/multi-word-component-names": "off",
+
+          },
+      }, ],
+  },
+};
+
